a2fbd3a01b3a0c2427fc369fd1405fa4e7b8bc15
Anna Rachel Valdez <annarachel.valdez@gmail.com>
2020-08-19 10:55:48 -0700

    basic css styling and adding async functionality

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello')
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+
+async function selectMediaStream() {
+    try {
+        something
+    } catch (error) {
+        console.log('error here:', error)
+
+    }
+}
+
+selectMediaStream();
