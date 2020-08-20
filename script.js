f48c155735cdc8e2b726e75f70c7f7dd19fbd92b
Anna Rachel Valdez <annarachel.valdez@gmail.com>
2020-08-20 12:15:30 -0700

    ui completed

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 const videoElement = document.getElementById('video');
-const button = document.getElementById('button');
+const selectShareScreen = document.getElementById('select-share-screen-button');
+const stopCaptureButton = document.getElementById('stop-capture-button');
 
-// Prompt to select media stream, pass to video element, then play
 
-async function selectMediaStream() {
+stopScreenCapture = () => {
+    let tracks = videoElement.srcObject.getTracks();
+    tracks.forEach(track => track.stop());
+    videoElement.srcObject = null;
+    selectShareScreen.disabled = false;
+    selectShareScreen.hidden = false;
+    stopCaptureButton.hidden = true;
+}
+
+selectShareScreen.addEventListener('click', async () => {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
         videoElement.srcObject = mediaStream;
         videoElement.onloadedmetadata = () => {
             videoElement.play();
+            startPictureInPicture();
         }
+        selectShareScreen.disabled = true;
+        selectShareScreen.hidden = true;
+        stopCaptureButton.disabled = false;
+        stopCaptureButton.hidden = false;
     } catch (error) {
-        console.log('error here:', error)
-
+        console.log('Error: ', error)
     }
+});
+
+stopCaptureButton.addEventListener('click', async () => {
+    stopCaptureButton.disabled = true;
+    stopScreenCapture();
+    stopPictureInPicture();
+})
+
+videoElement.addEventListener('leavepictureinpicture', function() {
+    stopScreenCapture(); 
+  });
+
+
+async function startPictureInPicture () {
+    try {
+        await videoElement.requestPictureInPicture();
+    } catch (error) {
+       console.log('Error: ', error)
+   }
 }
 
-
-button.addEventListener('click', async () => {
-    //Disable button
-    button.disabled = true;
-    //Start Picture in Picture
-    await videoElement.requestPictureInPicture();
-    //Reset Button
-    button.disabled = false;
-} );
-
-selectMediaStream();
+async function stopPictureInPicture () {
+    try {
+        await document.exitPictureInPicture();
+    } catch (error) {
+        console.log('Error:', error)
+    }
+}
